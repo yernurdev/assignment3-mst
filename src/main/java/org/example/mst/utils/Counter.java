a416f7c4ec6a67c55e3a00d2c8de62ee423aaaa8
Yernur <ernursyrlibayev@gmail.com>
2025-10-26 16:08:16 +0500

    utils

--- src/main/java/org/example/mst/utils/Counter.java
+++ src/main/java/org/example/mst/utils/Counter.java
@@ -1,4 +1,8 @@
 package org.example.mst.utils;
 
 public class Counter {
+    private long count = 0L;
+    public void inc() { count++; }
+    public void add(long k) { count += k; }
+    public long get() { return count; }
 }
